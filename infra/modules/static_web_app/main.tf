645c028727de19ccb1321777ba618280634cf982
mgarciap <manu230292@outlook.es>
2026-01-21 16:12:02 +0000

    refactor(terraform): remove unused Container Apps and Static Web App modules

--- infra/modules/static_web_app/main.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_static_web_app" "main" {
-  name                = "swa-${var.name}"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  sku_tier            = var.sku_tier
-  sku_size            = var.sku_size
-
-  tags = var.tags
-}

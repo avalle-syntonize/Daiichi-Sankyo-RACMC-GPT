8a3daf16872b6a09ab448b3d63dff3e52768a3b4
mgarciap <manu230292@outlook.es>
2026-02-04 22:04:31 +0000

    feat: Add BASE_API_URL to app settings in static web app configuration

--- infra/modules/static_web_app/main.tf
+++ infra/modules/static_web_app/main.tf
@@ -5,5 +5,8 @@ resource "azurerm_static_web_app" "main" {
   sku_tier            = var.sku_tier
   sku_size            = var.sku_size
 
+  app_settings = { 
+    "BASE_API_URL" = "https://racmc-gpt-dev-func-backend.azurewebsites.net/api"
+   }
   tags = var.tags
 }
